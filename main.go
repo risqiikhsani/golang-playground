bfa047f754fa4f344bf24c24d66b08e7e34bc395
risqi2 <risqiikhsani12@gmail.com>
2023-05-11 08:42:10 +0700

    test

--- main.go
+++ main.go
@@ -21,8 +21,14 @@ func main() {
 	mytry.Print("Bob", 5, "brown")
 	mytry.Print("Blacki", 5, "black")
 
-	var a [5]Car
+	var a [5]Car // this is array
 	a[0] = Car{"M4", "BMW", "sedan", "manual"}
 	a[1] = Car{"Civic", "Honda", "Sedan", "manual"}
 	fmt.Println(a)
+
+	var b []Car // this is slice
+	var1 := Car{"M4 2020", "BMW", "sedan", "manual"}
+	var2 := Car{"Civic 1999", "Honda", "Sedan", "manual"}
+	b = append(b, var1, var2, Car{"Civic 2019", "Honda", "Sedan", "manual"})
+	fmt.Println(b)
 }
