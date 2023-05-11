482b64da1f17ab013cec2aaee159d768cf77a9c9
risqi2 <risqiikhsani12@gmail.com>
2023-05-11 08:24:11 +0700

    test

--- main.go
+++ main.go
@@ -7,10 +7,22 @@ import (
 	mytry "example.com/test/try"
 )
 
+type Car struct {
+	name       string
+	publisher  string
+	car_type   string
+	drive_type string
+}
+
 func main() {
 	fmt.Println("Hi")
 	fmt.Println(mymath.Add(2, 2))
 	// mytry.Pointer()
 	mytry.Print("Bob", 5, "brown")
 	mytry.Print("Blacki", 5, "black")
+
+	var a [5]Car
+	a[0] = Car{"M4", "BMW", "sedan", "manual"}
+	a[1] = Car{"Civic", "Honda", "Sedan", "manual"}
+	fmt.Println(a)
 }
